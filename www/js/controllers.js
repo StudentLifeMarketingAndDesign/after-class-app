860466e3d09d5caa0c7b3a09f57f774fbc9ae705
shaanamin13 <shaan-amin@uiowa.edu>
2015-07-22 11:32:22 -0500

    After-Class feed of events

---
+++ www/js/controllers.js
@@ -0,0 +1,60 @@
+angular.module('starter.controllers', [])
+
+// .controller('FeedCtrl', function($scope, $http) {
+//   $http.get("http://hulk.imu.uiowa.edu/after-class/events/feed/")
+//   .then(function(res){
+//         console.log(res.data.events);
+//         $scope.events = res.data.events;    
+//         });
+
+// })
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+
+
+.controller('FeedCtrl', function(Events, $scope){
+
+ Events.async().then(function(res) { 
+    console.log(res.data.events);
+    $scope.events = res.data.events;
+  });
+
+})
+
+.controller('SingleEventCtrl', function($scope, $stateParams, Events){
+  Events.get($stateParams.eventId).then(function(res) { 
+    console.log(res.data.events);
+    $scope.event = res.data.event;
+  });
+
+
+
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
+
+
+
+
